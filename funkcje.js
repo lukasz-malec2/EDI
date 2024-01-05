4fca2fffd43166e64c801086fe1f81184b7a2d4d
lukasz1_malec <lmalec493@gmail.com>
2024-01-05 19:18:26 +0100

    going live

--- funkcje.js
+++ funkcje.js
@@ -32,6 +32,40 @@ function Fetch_and_display_data() {
 
 
 
+function Fetch_and_display_table() {
+  fetch("https://my.api.mockaroo.com/volleyballplayers.json?key=c097ba40")
+    .then(function(response){
+      return response.json()
+    })
+  .then(function(datapoints) {
+    let placeholder = document.getElementById("Fetch_retrieved_data_table");
+    let out = "";
+    for (let datapoint of datapoints) {
+      out += `
+        <tr>
+          <td>${datapoint.first_name}</td>
+          <td>${datapoint.last_name}</td>
+          <td>${datapoint.date_of_birth}</td>
+          <td>${datapoint.country_of_birth}</td>
+          <td>${datapoint.height_cm}</td>
+          <td>${datapoint.weight_kg}</td>
+          <td>${datapoint.position_in_field}</td>
+          <td>${datapoint.range_in_block}</td>
+          <td>${datapoint.range_in_attack}</td>
+          <td>${datapoint.curent_club}</td>
+        <tr>
+      `;
+    };
+    placeholder.innerHTML = out
+  })
+}
+
+
+
+
+
+
+
 
 
 //chart placeholder
@@ -107,4 +141,77 @@ function chart_js_method() {
   });
 }
 
+
+
+
+function chart_js_method2() {
+
+//chart2 placeholder
+const data_pos = {
+  labels: ['Position Placeholder'],
+  datasets: [{
+    label: '#Label Placeholder',
+    data: [1],
+    borderWidth: 2,
+//    backgroundColor: Object.values(Utils.CHART_COLORS),
+    }],
+  }
+const config_pos = {
+  type: 'doughnut',
+  data: data_pos,
+  options: {
+    plugins: {
+      colors: {
+        forceOverride: true
+      }
+    }
+  }
+}
+const players_by_position_chart = new Chart(document.getElementById("players_by_position_chart"),config_pos);
+
+  //Fetch block
+    async function fetch_data() {
+      const url = "https://my.api.mockaroo.com/volleyballplayers.json?key=c097ba40"; // full API 100 records
+  //    const url = "https://my.api.mockaroo.com/VolleyballPlayers2.json?key=c097ba40"; //simplified API 10 records less countries
+      const response = await fetch(url);
+      //wait until the request has been completed
+      const datapoints = await response.json(); //parsing json for js
+      console.log(datapoints)
+      return datapoints
+    };
+
+    fetch_data().then(datapoints => {
+      const position = datapoints.map(
+        function(index){
+          return index.position_in_field;
+        });
+      const uniqueposition = [...new Set(position)]; //filterining all positions to only unique ones
+      //console.log(country);
+      console.log(uniqueposition);
+      console.log(uniqueposition.length);
+
+      var pos_dict = {};
+      var position_count_array = []
+        for (var i = 0; i < uniqueposition.length; i++) {
+            var position_counter = 0
+          for (var j = 0; j < datapoints.length; j++) {
+            if (uniqueposition[i] == datapoints[j].position_in_field) {
+              position_counter = position_counter + 1
+            };
+          };
+          pos_dict[uniqueposition[i]] = position_counter;
+          position_count_array[i]=position_counter;
+        };
+        console.log(pos_dict);
+        console.log(position_count_array);
+
+      //update the labels in chart placeholder
+      players_by_position_chart.config.data.datasets[0].label = "No. of players in TOP 100";
+      players_by_position_chart.config.data.labels = uniqueposition;
+      players_by_position_chart.config.data.datasets[0].data = position_count_array;
+      players_by_position_chart.update()
+      console.log(players_by_position_chart.config)
+
+    });
+  }
 
